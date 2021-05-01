db168ab92765ce584fbdf2ab1825aec3cec4a59a
shabi983 <83406565+shabi983@users.noreply.github.com>
2021-05-01 13:22:24 -0700

    Update signup-user.php

--- signup-user.php
+++ signup-user.php
@@ -1,17 +1,17 @@
-<?php require_once "controllerUserData.php"; ?>
+<?php require_once "https://github.com/shabi983/SignUp/blob/main/controllerUserData.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Signup Form</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://github.com/shabi983/SignUp/blob/main/style.css">
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-4 offset-md-4 form">
-                <form action="signup-user.php" method="POST" autocomplete="">
+                <form action="https://github.com/shabi983/SignUp/blob/main/signup-user.php" method="POST" autocomplete="">
                     <h2 class="text-center">Signup Form</h2>
                     <p class="text-center">It's quick and easy.</p>
                     <?php
@@ -54,7 +54,7 @@
                     <div class="form-group">
                         <input class="form-control button" type="submit" name="signup" value="Signup">
                     </div>
-                    <div class="link login-link text-center">Already a member? <a href="login-user.php">Login here</a></div>
+                    <div class="link login-link text-center">Already a member? <a href="https://github.com/shabi983/SignUp/blob/main/login-user.php">Login here</a></div>
                 </form>
             </div>
         </div>
